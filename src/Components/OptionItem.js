1fb329db50410ae032a8b40f1e199fc5fe1c119b
Dhansuhu Uzumaki <a.p.dhanushu@gmail.com>
2018-01-05 21:26:49 +0530

    added delete functionality

--- src/Components/OptionItem.js
+++ src/Components/OptionItem.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableNativeFeedback } from 'react-native';
 import PropTypes from 'prop-types';
 import appTheme from '../assets/colors.json';
 
 const styles = StyleSheet.create({
-  viewStyle: {
-    minWidth: '100%',
+  outerViewStyle: {
+    borderColor: 'transparent',
+    borderRadius: 10,
+    backgroundColor: appTheme.primaryLight,
+    flexDirection: 'row',
+    flex: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+    margin: 5
+  },
+  innerViewStyle: {
+    minWidth: '78%',
+    width: '95%',
     minHeight: 40,
     borderWidth: 2,
     padding: 15,
@@ -16,16 +27,51 @@ const styles = StyleSheet.create({
   textStyle: {
     fontSize: 18,
     color: appTheme.primaryText
+  },
+  deleteButtonStyle: {
+    backgroundColor: appTheme.secondary,
+    height: 20,
+    width: 20,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50
+  },
+  deleteButtonTextStyle: {
+    fontSize: 12,
+    color: appTheme.secondaryText,
+    textAlign: 'center',
+    bottom: 1
   }
 });
 
 class OptionItem extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.delete = () => this._delete();
+  }
+
+  _delete () {
+    this.props.onDelete(this.props.id);
+  }
+
   render () {
     return (
-      <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>
-          {this.props.text}
-        </Text>
+      <View style={styles.outerViewStyle}>
+        <View style={styles.innerViewStyle}>
+          <Text style={styles.textStyle}>
+            {this.props.text}
+          </Text>
+        </View>
+        <TouchableNativeFeedback
+          onPress={this.delete}
+          background={TouchableNativeFeedback.SelectableBackground()}
+        >
+          <View style={styles.deleteButtonStyle}>
+            <Text style={styles.deleteButtonTextStyle}>x</Text>
+          </View>
+        </TouchableNativeFeedback>
       </View>
     );
   }
@@ -33,8 +79,8 @@ class OptionItem extends React.Component {
 
 OptionItem.propTypes = {
   text: PropTypes.string,
-  // value: PropTypes.string,
-  // onDelete: PropTypes.func
+  onDelete: PropTypes.func,
+  id: PropTypes.number
 };
 
 export default OptionItem;
